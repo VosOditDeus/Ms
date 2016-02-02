7ebc1294e293d53e89def457653ae0c183baad1d
vosoditdeus <vosoditdeus@gmail.com>
2016-02-02 21:13:01 +0200

    some custom changes,readability, etc.

--- photo/views.py
+++ photo/views.py
@@ -1,31 +1,23 @@
-# from django.contrib import messages
-from django.core.files.uploadedfile import UploadedFile
+
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseBadRequest
-from django.shortcuts import render_to_response, redirect, HttpResponse, HttpResponseRedirect, get_object_or_404,render
+from django.shortcuts import render_to_response, redirect, HttpResponseRedirect, get_object_or_404,render
 from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
-from django.template import RequestContext
-from sorl.thumbnail import get_thumbnail
-
-from Ms import settings
 from Ms.settings import MEDIA_URL
 from models import *
 from forms import *
 from datetime import datetime
 from django.core.mail import send_mail
 from Ms.local_settings import EMAIL_HOST_USER
-import simplejson
-
 # coding: utf-8
 def God(request):
-    categories = Categories.objects.all()
-    images = Image.objects.all().filter(approved=True)
-    last_photos = images.filter(created=datetime.today())
+    categories_list = Categories.objects.all()
+    images_filtered_list = Image.objects.all().filter(approved=True)
+    last_photos_list = images_filtered_list.filter(created=datetime.today())
     args = {}
     args.update(csrf(request))
-    paginator = Paginator(images, 7)
+    paginator = Paginator(images_filtered_list, 7)
     try:
         page = int(request.GET.get("page", '1'))
     except ValueError:
@@ -34,11 +26,11 @@ def God(request):
         images = paginator.page(page)
     except (InvalidPage, EmptyPage):
         images = paginator.page(paginator.num_pages)
-    args['categories'] = categories
+    args['categories'] = categories_list
     args['images'] = images
     args['user'] = request.user
     args['media_url'] = MEDIA_URL
-    args['last'] = last_photos
+    args['last'] = last_photos_list
     return render_to_response("base.html", args)
 
 
@@ -49,47 +41,36 @@ def addPhoto(request):
     args = {}
     args.update(csrf(request))
     form = PhotoForm(request.POST or None, request.FILES or None)
-    if request.method == "POST":
-        form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            img = form.save(commit=False)
-            img.user = request.user
-            # title = form.cleaned_data.get("title")
-            # if not img.title:
-            #     title = '#'
-            # img.title = title
-            img.save()
-            form.save_m2m()
-            return HttpResponseRedirect(reverse('addPhoto'))
-        else:
-            args['form'] = form
-            return render_to_response('addphoto.html', args)
-    else:
-        form = PhotoForm()
-        args['form'] = form
+    if form.is_valid():
+        img = form.save(commit=False)
+        img.user = request.user
+        img.save()
+        form.save_m2m()
+        return HttpResponseRedirect(reverse('addPhoto'))
+    args['form']=form
     return render_to_response('addphoto.html', args)
 
 
 def addlike(request, img_id):
     if img_id:
-        a = get_object_or_404(Image, id=img_id)
-        ifliked = a.liked_persons.filter(username=request.user).exists()
+        image = get_object_or_404(Image, id=img_id)
+        ifliked = image.liked_persons.filter(username=request.user).exists()
         if not ifliked:
-            a.liked_persons.add(request.user)
-            a.likes += 1
-            a.save()
+            image.liked_persons.add(request.user)
+            image.likes += 1
+            image.save()
         else:
-            a.liked_persons.remove(request.user)
-            a.likes -= 1
-            a.save()
+            image.liked_persons.remove(request.user)
+            image.likes -= 1
+            image.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def show_your_pictures(request):
     args = {}
-    img = Image.objects.all().filter(user=request.user)
-    da = img.filter(created=datetime.today())
-    args['image'] = img
+    image_list = Image.objects.all().filter(user=request.user)
+    da = image_list.filter(created=datetime.today())
+    args['image'] = image_list
     args['da'] = da
     return render_to_response('ypic.html', args)
 
@@ -114,7 +95,7 @@ def update(request,id=None):
         img.save()
         #messages.success(request, "Updated")
         return HttpResponseRedirect('/')#TODO: Make an absolute url to models and rewrite this shit
-    context={
+    context = {
             "instance": instance,
             "form": form
         }
@@ -122,14 +103,14 @@ def update(request,id=None):
 
 
 def categories_detail(request, cat_pk):
-    Cat = get_object_or_404(Categories,pk=cat_pk)
-    images = Cat.images.all()
+    categories_list = get_object_or_404(Categories,pk=cat_pk)
+    images_list = categories_list.images.all()
     if not request.user.is_authenticated():
-        images = images.filter(approved=True)
+        images_list = images_list.filter(approved=True)
     args = {}
     args.update(csrf(request))
-    args['cat'] = Cat
-    args['images'] = images
+    args['cat'] = categories_list
+    args['images'] = images_list
     args['backurl'] = request.META.get("HTTP_REFERER")
     args['media_url'] = MEDIA_URL
     args['user'] = request.user
@@ -154,7 +135,7 @@ def contact(request):
     return render(request, 'cus.html', context)
 #TODO: test it on any other environment
 def delete(request,id=None):
-    instance=get_object_or_404(Image, id=id)
+    instance = get_object_or_404(Image, id=id)
     os.remove(instance.image.path)
     instance.delete()
     #TODO: need to change it on album reverse absolute ulr, i think
